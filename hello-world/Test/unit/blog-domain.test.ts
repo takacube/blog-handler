c03a56effe7ea64971d14af5ef498965d45ce55a
taka <shimakuratakanao@gmail.com>
2022-08-22 22:45:18 +0900

    :sparkles: first commit

---
+++ hello-world/Test/unit/blog-domain.test.ts
@@ -0,0 +1,10 @@
+describe('Domain', () => {
+    test('blog', () => {
+        const five = '5';
+        expect(five).toEqual('5');
+    });
+    test('a', () => {
+        const five = '5';
+        expect(five).toEqual('5');
+    });
+});
