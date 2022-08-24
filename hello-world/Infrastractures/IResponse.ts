ad1156afa139034b3eb541ac062c2d30b951b811
taka <shimakuratakanao@gmail.com>
2022-08-24 22:32:47 +0900

    :hammer: add infra layer

---
+++ hello-world/Infrastractures/IResponse.ts
@@ -0,0 +1,6 @@
+export interface IResponse {
+        genre: {S: string},
+        url: {S: string},
+        comment: {S: string}
+        error: {S: string}
+}
--- hello-world/Infrastractures/MockBlogStorage.ts
+++ hello-world/Infrastractures/MockBlogStorage.ts
@@ -1,14 +1,30 @@
 import { Blog } from '../Domains/Blog'
 import { Genre } from '../Domains/Genre'
 import { IBlog } from '../Domains/IBlog'
+import { IResponse } from './IResponse'
 export class MockBlogStorage {
     findByGenre(genre: Genre){
-        return []
+        return [{
+            genre: {S: 'frontend'},
+            url: {S: 'https://takanao.com'},
+            comment: {S: 'comment--'},
+            error: {S: 'a'}
+        }]
     }
     findByUrl(url: string) {
         if(url == 'https://takanao.com'){
-            return {url: 'https://takanaos.com', comment: 'thanks', genre: 'frontend'}
+            return {
+                genre: {S: 'frontend'},
+                url: {S: 'https://takanao.com'},
+                comment: {S: 'thanks'},
+                error: {S: ''}
+            }
         }
-        return false
+        return {
+            genre: {S: ''},
+            url: {S: ''},
+            comment: {S: ''},
+            error: {S: 'エラーメッセージ'}
+        }
     }
 }
