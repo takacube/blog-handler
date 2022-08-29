c40949fc9ec894b65d110f2437277774b0f79944
taka <shimakuratakanao@gmail.com>
2022-08-29 20:49:24 +0900

    add controller

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -1,5 +1,7 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { getBlogList } from './Presentaion/CLI/GetBlog'
+import { getBlogList } from './Presentaion/CLI/GetBlog';
+import axios from 'axios';
+import { handleRequest } from './Presentaion/LINE/controller'
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -12,24 +14,27 @@ import { getBlogList } from './Presentaion/CLI/GetBlog'
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     let response: APIGatewayProxyResult;
-    const res = await getBlogList('frontend')
-
     try {
+        if(event['body']){
+            const httpBody = JSON.parse(event['body'])
+            const replyToken = httpBody['events'][0]["replyToken"]
+            const receivedMsg = httpBody['events'][0]["message"]["text"]
+            await handleRequest(receivedMsg, replyToken)
+        }
         response = {
             statusCode: 200,
             body: JSON.stringify({
-                message: `レスポンス${res}`,
+                message: 'successfully end',
             }),
         };
     } catch (err) {
         console.log(err);
-        response = {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: `some error happened${err}`,
-            }),
-        };
+            response = {
+                statusCode: 500,
+                body: JSON.stringify({
+                    message: `some error happened${err}`,
+                }),
+            }
     }
-
     return response;
 };
